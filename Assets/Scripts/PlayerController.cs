be3fcd9de50d072a24b0710450211c653add1ac9
lil-hummonkey <112558499+lil-hummonkey@users.noreply.github.com>
2023-09-11 13:20:55 +0200

    1 bobo initial commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
   [SerializeField]
   float movementSpeed = 3f; // Unity-enheter per sekund
@@ -19,8 +20,21 @@ public class PlayerController : MonoBehaviour {
   [SerializeField]
   float timeBetweenShots = 0.5f;
   float timeSinceLastShot = 0f;
+
+  void Start()
+  {
+    if (isLocalPlayer)
+    {
+      GetComponent<Renderer>().material.color = Color.green;
+    }
+  }
 	
 	void Update () {
+
+    if(!isLocalPlayer)
+    {
+      return;
+    }
 
     float yRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
     float zMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
@@ -44,8 +58,11 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
+[Command]
   void Fire()
   {
     GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+    NetworkServer.Spawn(bullet);
   }
 }
